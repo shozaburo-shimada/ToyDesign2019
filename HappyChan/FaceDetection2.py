c0bdc2a6201323e83c8123522ca338f15786f527
Shozaburo Shimada <shozaburo@piccolo.co>
2019-11-24 13:10:05 +0900

    fix error

--- HappyChan/FaceDetection2.py
+++ HappyChan/FaceDetection2.py
@@ -21,14 +21,17 @@ class DetectThread(threading.Thread):
         detectedFaces = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml').detectMultiScale(
             gray,
             scaleFactor=1.1,
-            minNeighbors=3,
-            minSize=(30, 30),
-            flags=cv.CV_HAAR_SCALE_IMAGE
+            minNeighbors=5,
+            minSize=(20, 20),
+            #minNeighbors=3,
+            #minSize=(30, 30),
+            #flags=cv.CV_HAAR_SCALE_IMAGE
         )
         self.faces[:] = detectedFaces
 
 while True:
     _, img = capture.read()
+    img = cv2.flip(img, -1)
     img = cv2.resize(img, (320, 240))
     if count == 30:
         thread = DetectThread(img, faces)
