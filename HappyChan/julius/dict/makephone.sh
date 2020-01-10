faf4a644f68064da47d2fce7b757d8134f856fff
Shozaburo Shimada <shozaburo@piccolo.co>
2020-01-10 20:07:29 +0900

    update

---
+++ HappyChan/julius/dict/makephone.sh
@@ -0,0 +1,2 @@
+iconv -f utf8 -t eucjp ~/Documents/ToyDesign2019/HappyChan/julius/dict/happy.yomi | ~/Documents/ToyDesign2019/HappyChan/julius/julius-4.4.2.1/gramtools/yomi2voca/yomi2voca.pl | iconv -f eucjp -t utf8 > ~/Documents/ToyDesign2019/HappyChan/julius/dict/happy.phone
+
