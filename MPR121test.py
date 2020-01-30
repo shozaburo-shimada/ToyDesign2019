64e38ac874a2c1438c589588a31f05346d5ce7b0
Shozaburo Shimada <shozaburo@piccolo.co>
2020-01-30 17:42:10 +0900

    Update MPR121test.py

--- MPR121test.py
+++ MPR121test.py
@@ -23,14 +23,21 @@ while True:
     for i in range(12):
 
         pin_bit = 1 << i
-
+        
+        # i番目のポートが、前回タッチされてなくて、かつ今回タッチされたときだけ = タッチを検出
         if current_touched & pin_bit and not last_touched & pin_bit:
             print('curr: {:012b}'.format(current_touched))
             print('last: {:012b}'.format(last_touched))
 
             print('{0} touched!\n'.format(i))
+            if i == 0:
+                # 0番目がタッチされた
+                # movie0.mp4を再生
+            if i == 1:
+                # 2番目がタッチされた
+                # movie1.mp4を再生
 
-
+        # i番目のポートが、前回までタッチされていて、かつ今回タッチされていない = リリースを検出
         if not current_touched & pin_bit and last_touched & pin_bit:
             print('curr: {:012b}'.format(current_touched))
             print('last: {:012b}'.format(last_touched))
