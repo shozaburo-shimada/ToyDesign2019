20366e6125df1350c104c5af21344121a9b5ef45
Shozaburo Shimada <shozaburo@piccolo.co>
2020-01-31 17:20:15 +0900

    gatt server example

--- HappyChan/bleserver.py
+++ HappyChan/bleserver.py
@@ -1,13 +1,100 @@
-
-from RN42 import RN42
+from pybleno import *
 import time
 
-# Confirm Bluetooth MAC Address with "$ hciconfig" command
-ras = RN42("ras", "B8:27:EB:20:C2:96 ", 1)
-ras.connectBluetooth(ras.bdAddr, ras.port)
+bleno = Bleno()
+
+APPROACH_SERVICE_UUID = '13A28130-8883-49A8-8BDB-42BC1A7107F4'
+APPROACH_CHARACTERISTIC_UUID = 'A2935077-201F-44EB-82E8-10CC02AD8CE1'
+
+
+class ApproachCharacteristic(Characteristic):
+
+    def __init__(self):
+        Characteristic.__init__(self, {
+            'uuid': APPROACH_CHARACTERISTIC_UUID,
+            'properties': ['read', 'notify', 'write'],
+            'value': None
+        })
+
+        self._value = 0
+        self._updateValueCallback = None
+
+    def onReadRequest(self, offset, callback):
+        print()
+        callback(Characteristic.RESULT_SUCCESS, self._value)
+
+    def onSubscribe(self, maxValueSize, updateValueCallback):
+        print('ApproachCharacteristic - onSubscribe')
+
+        self._updateValueCallback = updateValueCallback
+
+    def onUnsubscribe(self):
+        print('ApproachCharacteristic - onUnsubscribe')
+
+        self._updateValueCallback = None
+
+    def onWriteRequest(self, data, offset, withoutResponse, callback):
+        self._value = data
+        global counter
+        counter  = int.from_bytes(data, 'big')
+        print('Write Request: ' + str(counter))
+
+        callback(Characteristic.RESULT_SUCCESS)
+
+def onAccept(clientAddress):
+    print('Accept: ' + clientAddress)
+
+def onDisconnect(clientAddress):
+    print('disconnect: ' + clientAddress)
+
+def onStateChange(state):
+    print('on -> stateChange: ' + state)
+
+    if (state == 'poweredOn'):
+        bleno.startAdvertising('Approach', [APPROACH_SERVICE_UUID])
+    else:
+        bleno.stopAdvertising()
+
+
+bleno.on('stateChange', onStateChange)
+bleno.on('accept', onAccept)
+bleno.on('disconnect', onDisconnect)
+
+approachCharacteristic = ApproachCharacteristic()
+
+
+def onAdvertisingStart(error):
+    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'))
+
+    if not error:
+        bleno.setServices([
+            BlenoPrimaryService({
+                'uuid': APPROACH_SERVICE_UUID,
+                'characteristics': [
+                    approachCharacteristic
+                ]
+            })
+        ])
+
+
+bleno.on('advertisingStart', onAdvertisingStart)
+
+bleno.start()
+
+counter = 0
+
+def task():
+    global counter
+    counter += 1
+    approachCharacteristic._value = counter
+    if approachCharacteristic._updateValueCallback:
+
+        print('Sending notification with value : ' + str(approachCharacteristic._value))
+
+        notificationBytes = str(approachCharacteristic._value).encode()
+        approachCharacteristic._updateValueCallback(notificationBytes)
+
 
 while True:
-  try:
-    ras.sock.send("Hello World")
-    print "send data"
-
+    task()
+    time.sleep(1)
