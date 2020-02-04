3345881e08f2efa784f5db0b3862d1894f26e15b
Shozaburo Shimada <shozaburo@piccolo.co>
2020-02-04 18:29:55 +0900

    Merge ble and voice recog into Happy3

---
+++ HappyChan/Happy3.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from pybleno import *
+import time
+import socket
+import subprocess
+
+
+class ApproachCharacteristic(Characteristic):
+
+    def __init__(self):
+        Characteristic.__init__(self, {
+            'uuid': APPROACH_CHARACTERISTIC_UUID,
+            'properties': ['read', 'notify', 'write'],
+            'value': None
+        })
+
+        self._value = 0
+        self._updateValueCallback = None
+
+    def onReadRequest(self, offset, callback):
+        print()
+        callback(Characteristic.RESULT_SUCCESS, self._value)
+
+    def onSubscribe(self, maxValueSize, updateValueCallback):
+        print('ApproachCharacteristic - onSubscribe')
+
+        self._updateValueCallback = updateValueCallback
+
+    def onUnsubscribe(self):
+        print('ApproachCharacteristic - onUnsubscribe')
+
+        self._updateValueCallback = None
+
+    def onWriteRequest(self, data, offset, withoutResponse, callback):
+        self._value = data
+        global counter
+        counter  = int.from_bytes(data, 'big')
+        print('Write Request: ' + str(counter))
+
+        callback(Characteristic.RESULT_SUCCESS)
+
+def onAccept(clientAddress):
+    print('Accept: ' + clientAddress)
+
+def onDisconnect(clientAddress):
+    print('disconnect: ' + clientAddress)
+
+def onStateChange(state):
+    print('on -> stateChange: ' + state)
+
+    if (state == 'poweredOn'):
+        bleno.startAdvertising('Approach', [APPROACH_SERVICE_UUID])
+    else:
+        bleno.stopAdvertising()
+
+def onAdvertisingStart(error):
+    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'))
+
+    if not error:
+        bleno.setServices([
+            BlenoPrimaryService({
+                'uuid': APPROACH_SERVICE_UUID,
+                'characteristics': [
+                    approachCharacteristic
+                ]
+            })
+        ])
+
+def init_servo():
+  pass
+
+def init_julius():
+  p = subprocess.Popen(["sh julius.sh"], stdout=subprocess.PIPE, shell=True)
+  pid = str(p.stdout.read().decode('utf-8'))
+  print("Julius PID: " + pid)
+  time.sleep(3)
+  return p, pid
+
+def init_sock():
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect((host, port))
+  return sock
+
+
+# Response
+def response(keyword):
+  #print(keyword)
+  if keyword == '勉強する':
+    print('勉強する')
+  
+  elif keyword == 'いただきます':
+    print('めしあがれ')
+
+  elif keyword == 'ごちそうさま':
+    print('ごちそうさま')
+
+  elif keyword == '疲れた':
+    print('がんばって')
+
+  elif keyword == '眠い':
+    print('起きろ')
+
+  elif keyword == 'お腹空いた':
+     print('何が食べたい？') 
+
+  elif keyword == 'この問題難しい':
+    print('がんばって')
+
+  elif keyword == '今から勉強するね':
+    print('がんばって')
+
+  elif keyword == '勉強終わったよ':
+    print('おつかれさま')
+
+  elif keyword == 'おはよう':
+    print('おはよう')
+
+  elif keyword == 'おやすみ':
+    print('また明日ね')
+
+
+
+def notify_task():
+  global counter
+  #counter += 1
+  approachCharacteristic._value = counter
+  if approachCharacteristic._updateValueCallback:
+
+    print('Sending notification with value : ' + str(approachCharacteristic._value))
+
+    notificationBytes = str(approachCharacteristic._value).encode()
+    approachCharacteristic._updateValueCallback(notificationBytes)
+
+# Global
+APPROACH_SERVICE_UUID = '13A28130-8883-49A8-8BDB-42BC1A7107F4'
+APPROACH_CHARACTERISTIC_UUID = 'A2935077-201F-44EB-82E8-10CC02AD8CE1'
+host = 'localhost'
+port = 10500
+
+counter = 0
+
+
+# Program start here
+if __name__ == "__main__":
+  print("Start Happy-chan")  
+
+  # Init BLE  
+  approachCharacteristic = ApproachCharacteristic()
+  bleno = Bleno()
+  bleno.on('stateChange', onStateChange)
+  bleno.on('accept', onAccept)
+  bleno.on('disconnect', onDisconnect)
+  bleno.on('advertisingStart', onAdvertisingStart)
+  bleno.start()
+
+  # Init Julius
+  p, pid = init_julius()
+
+  # Init Socket
+  sock = init_sock()  
+  res = ''
+
+  # Init Servo
+
+  print("End initialize")
+
+
+  #Main Loop
+  print("Start voice recognition")
+  try:
+    while True:
+
+      # Waiting for the end of sentence(='\n.')
+      while(res.find('\n.') == -1):
+        # Store the data from julius
+        res += str(sock.recv(1024).decode('utf-8'))
+
+      word = ''
+      for line in res.split('\n'):
+        index = line.find('WORD=')
+        # print('OK')
+
+        # If there is a word we want to get
+        if index != -1:
+          line = line[index + 6:line.find('"', index + 6)]
+          if line != '[s]':
+            word = word + line
+
+        #print(word)
+
+        # Response for keyword
+        response(word)
+
+        res = ''
+
+      #BLE Notify 
+      notify_task() 
+
+  except KeyboardInterrupt:
+    print("\nEnd Happy-chan")
+    p.kill()
+    subprocess.call(["kill " + pid], shell=True)
+    sock.close()
--- HappyChan/julius.sh
+++ HappyChan/julius.sh
@@ -1,13 +1,17 @@
 # Confirm priority of mic
 # Require "0 snd_usb_audio"
-cat /proc/asound/modules
+# cat /proc/asound/modules
 
 # Execute
 #julius -C ~/Documents/ToyDesign2019/HappyChan/julius/julius-kit/dictation-kit-v4.4/am-gmm.jconf -nostrip -gram ~/Documents/ToyDesign2019/HappyChan/julius/dict/happy -input mic
 
 # Execute as module mode
 #julius -C ~/Documents/ToyDesign2019/HappyChan/julius/julius-kit/dictation-kit-v4.4/am-gmm.jconf -nostrip -gram ~/Documents/ToyDesign2019/HappyChan/julius/dict/hello -input mic -module
-julius -C ~/Documents/ToyDesign2019/HappyChan/julius/julius-kit/dictation-kit-v4.4/am-gmm.jconf -nostrip -gram ~/Documents/ToyDesign2019/HappyChan/julius/dict/happy -input mic -module &
+julius -C /home/pi/Documents/ToyDesign2019/HappyChan/julius/julius-kit/dictation-kit-v4.4/am-gmm.jconf -nostrip -gram /home/pi/Documents/ToyDesign2019/HappyChan/julius/dict/happy -input mic -module > log.txt &
+
+# Get process ID
+echo $!
+sleep 2
 
 #python VoiceRecog.py
 
