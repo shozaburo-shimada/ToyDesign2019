dcd7a91b33dafa4e604c080b220eb3e82981b98c
Kameda <kameda@hoge.hoge>
2020-02-06 18:22:12 +0900

    music

--- MPR121test.py
+++ MPR121test.py
@@ -2,13 +2,13 @@
 
 import sys
 import time
-
+import subprocess
 import Adafruit_MPR121.MPR121 as MPR121
-
+import pygame
 
 
 cap = MPR121.MPR121()
-
+pygame.mixer.init()
 
 if not cap.begin():
     print('Error initializing MPR121.  Check your wiring!')
@@ -17,6 +17,10 @@ if not cap.begin():
 
 print('Press Ctrl-C to quit.')
 last_touched = cap.touched()
+flag_video1 = False
+flag_video2 = False
+flag_video3 = False
+music_num = 3
 while True:
     current_touched = cap.touched()
 
@@ -33,11 +37,55 @@ while True:
             #if i == 0:
                 # 0番目がタッチされた
                 # movie0.mp4を再生
-            #if i == 1:
-                # 2番目がタッチされた
-                # movie1.mp4を再生
+            if i == 1:
+              if flag_video2 == True or flag_video3 == True:
+                proc.stdin.write('q')
+                proc.stdin.flush()
+                flag_video2 = False
+                flag_video3 = False
+              if flag_video1 == False:
+                command = "sudo omxplayer --win '0 0 1280 720' video1.mp4"
+                proc = subprocess.Popen(command, shell=True, stdin =subprocess.PIPE)
+                flag_video1 = True 
+
+            if i == 2:
+              if flag_video1 == True or flag_video3 == True:
+                proc.stdin.write('q')
+                proc.stdin.flush()
+                flag_video1 = False
+                flag_video3 = False
+              if flag_video2 == False:
+                command = "sudo omxplayer --win '0 0 1280 720' video2.mp4"
+                proc = subprocess.Popen(command, shell=True, stdin =subprocess.PIPE)
+                flag_video2 = True 
+
+            if i == 3:
+              if flag_video1 == True or flag_video2 == True:
+                proc.stdin.write('q')
+                proc.stdin.flush()
+                flag_video1 = False
+                flag_video2 = False
+              if flag_video3 == False:              
+                command = "sudo omxplayer --win '0 0 1280 720' video3.mp4"
+                proc = subprocess.Popen(command, shell=True, stdin =subprocess.PIPE)
+                flag_video3 = True 
+
+            if i == 4:
+              if music_num == 3:
+                pygame.mixer.music.load("music1.mp3")
+                pygame.mixer.music.play(1)
+                music_num = 1
+              elif music_num == 1:
+                pygame.mixer.music.load("music2.mp3")
+                pygame.mixer.music.play(1)
+                music_num = 2
+              elif music_num == 2:
+                pygame.mixer.music.load("music3.mp3")
+                pygame.mixer.music.play(1)
+                music_num = 3
 
         # i番目のポートが、前回までタッチされていて、かつ今回タッチされていない = リリースを検出
+
         if not current_touched & pin_bit and last_touched & pin_bit:
             print('curr: {:012b}'.format(current_touched))
             print('last: {:012b}'.format(last_touched))
